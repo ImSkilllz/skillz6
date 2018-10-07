63cfcec7402584d927c43d2ceef5d8ef88ba9bf8
ImSkilllz <43930333+ImSkilllz@users.noreply.github.com>
2018-10-07 20:47:40 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+
+console.log("BOT ONLINE");
+
+
+
+
+client.on('message', message => {
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(سباام يولد يلعن اومم الفله ${x})
+          .then(m => {
+            count++;
+          })
+
+        }
+      }
+});
+
+client.login("NDk3ODQ2ODIxODg4OTgzMDUw.DplICA.Jow-FbtSca34gIFK38W3uzGPwic");
