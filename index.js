e0c388942ab76ef7c63704a57f8b20eb5bb351f6
ImSkilllz <43930333+ImSkilllz@users.noreply.github.com>
2018-10-28 15:07:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const tokenfile = require("./token.json");
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const hook = new Discord.WebhookClient('506090110509514752', 'tr8hhuUeYD3sAQnwkhcMQgWuvEH-XBO5pP4PuJukkoQGy2z9EiSW9zyX5MPwIlxc-rzl');
 
 console.log("BOT ONLINE");
 
@@ -10,7 +11,7 @@ if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send('سباام يولد يلعن اومم الفله')
+        hook.send('fuck!');
           .then(m => {
             count++;
           })
@@ -20,4 +21,3 @@ if (message.content === '!spam') {
 });
 
 
-cleint.login("NDc0ODg3MjE5Mzc5ODMwNzk1.DrcX2w.wm39DU3WcdBEPhhlNGbe9qLC_uE")
