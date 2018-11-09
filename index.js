81f03802ff2d762f6b00f5c2a991c9f684e255fc
ImSkilllz <43930333+ImSkilllz@users.noreply.github.com>
2018-11-09 14:49:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ if (message.content === 'spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send('k')
+        message.channel.send('rytbw46uy4u8w45678ba47bq4a7q456bq34a6v54quw57n8i35w7m98e69n73ew3uwwbv47yvq3b76    367NQ5745M68W5N8W58NW568W5678W657BW47W457W47k')
           .then(m => {
             count++;
           })
